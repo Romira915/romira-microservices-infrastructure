e2ef7a17e632c0cdae13ce562c0390a99d6718d8
Romira915 <40430090+Romira915@users.noreply.github.com>
2023-07-18 15:38:13 +0000

    add: data

--- terraform/oci/osaka-always-free/data.tf
+++ terraform/oci/osaka-always-free/data.tf
@@ -1,3 +1,7 @@
 data "oci_identity_availability_domains" "ads" {
   compartment_id = var.compartment_id
 }
+
+data "oci_objectstorage_namespace" "ray_namespace" {
+  compartment_id = var.compartment_id
+}
