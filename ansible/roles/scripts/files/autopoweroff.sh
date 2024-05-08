6345d9172b5cd63834bb8fe138a3512083472257
Romira915 <40430090+Romira915@users.noreply.github.com>
2024-05-08 19:47:53 +0900

    fix: recive rtc mode

--- ansible/roles/scripts/files/autopoweroff.sh
+++ ansible/roles/scripts/files/autopoweroff.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-/usr/bin/curl -s https://holidays-jp.github.io/api/v1/date.json | jq -s -r '[ .[] | keys ] | flatten | unique | .[]' | grep $(date '+%Y-%m-%d') || rtcwake -m off -s 18000
+mode="off"
+
+# 引数をチェック
+while getopts "m:" opt; do
+  case ${opt} in
+    m )
+      mode="$OPTARG"
+      ;;
+    \? )
+      echo "Invalid option: $OPTARG" 1>&2
+      exit 1
+      ;;
+    : )
+      echo "Option -$OPTARG requires an argument." 1>&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
+/usr/bin/curl -s https://holidays-jp.github.io/api/v1/date.json | jq -s -r '[ .[] | keys ] | flatten | unique | .[]' | grep $(date '+%Y-%m-%d') || rtcwake -m $mode -s 18000
